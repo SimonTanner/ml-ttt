17908949c5699714d596c5dcd6116e3782e22de5
SimonTanner <simonthetanner@hotmail.com>
2018-02-11 15:23:23 +0000

    Attempts to fix build failure due to collect-static not working by adding these settings directly into prod_settings.

--- tictactoe/settings/prod_settings.py
+++ tictactoe/settings/prod_settings.py
@@ -6,3 +6,16 @@ db_from_env = dj_database_url.config()
 DEBUG = False
 
 DATABASES['default'].update(db_from_env)
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'ttt/static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
