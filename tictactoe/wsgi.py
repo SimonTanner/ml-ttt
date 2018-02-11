681a03c03e99358746fab00ec58dac2f1911217a
SimonTanner <simonthetanner@hotmail.com>
2018-02-11 16:29:36 +0000

    Found error in wsgi.py where the settings file specified had .py on the end :(

--- tictactoe/wsgi.py
+++ tictactoe/wsgi.py
@@ -12,7 +12,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tictactoe.settings.prod_settings.py")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tictactoe.settings.prod_settings")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
